e857ded4e12faf11a01669d895abb2a5e997ca8d
wb430 <2562105152@QQ.com>
2026-06-18 15:07:58 +0800

    Add logging for user retrieval test

--- src/test/java/com/example/SpringBoot/controller/TestUserController.java
+++ src/test/java/com/example/SpringBoot/controller/TestUserController.java
@@ -64,7 +64,7 @@
      public void apiTest02() throws Exception {
         MvcResult ra = mockMvc.perform(MockMvcRequestBuilders.get("/user/getUserByName/wb"))
                 .andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
-
+        System.out.println("Dev分支操作");
         logger.info("状态响应码{}:"+ra.getResponse().getStatus());
         logger.info("响应结果{}:"+ra.getResponse().getContentAsString());
 
